0c7bf07d058a395600f3c4f3efbfb6f11843b254
Tim Kelly <timpkelly1@gmail.com>
2021-02-15 08:03:03 +1100

    Added semantic-ui react and implemented a section with QuickQuote.js

--- src/pages/QuickQuote.js
+++ src/pages/QuickQuote.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
-const QuickQuote = () => (
-  <div>
-    <h1>Example heading</h1>
-    <a href="./google">example link</a>
-  </div>
+import { Button, Checkbox, Form } from 'semantic-ui-react';
+
+const FormExampleForm = () => (
+  <Form>
+    <Form.Field>
+      <label htmlFor="first-name">
+        First Name
+        <input placeholder="First Name" id="first-name" />
+      </label>
+    </Form.Field>
+    <Form.Field>
+      <label htmlFor="last-name">
+        Last Name
+        <input placeholder="Last Name" id="last-name" />
+      </label>
+    </Form.Field>
+    <Form.Field>
+      <Checkbox label="I agree to the Terms and Conditions" />
+    </Form.Field>
+    <Button type="submit">Submit</Button>
+  </Form>
 );
 
-export default QuickQuote;
+export default FormExampleForm;
